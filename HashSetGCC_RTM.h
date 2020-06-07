dda257872cb463409f3eee2deaba4a00843ad5ea
Michal Guzek <mguzek@uci.edu>
2020-06-07 11:17:51 -0700

    Final fixes

--- HashSetGCC_RTM.h
+++ HashSetGCC_RTM.h
@@ -29,7 +29,7 @@ protected:
                 _xabort(0xFF);
             }
             if((status & _XABORT_EXPLICIT) && _XABORT_CODE(status) == 0xff) {
-                std::this_thread::sleep_for(std::chrono::nanoseconds((dist(rng) % RTM_WAITNSEC) + 1));
+                std::this_thread::sleep_for(std::chrono::nanoseconds(dist(rng)));
             }
             else if(!(status & _XABORT_RETRY)) {
                 break;
@@ -41,12 +41,11 @@ protected:
     }
 
     std::mt19937 rng;
-    std::uniform_int_distribution<std::mt19937::result_type> dist; //default range is [0, MAX]
+    std::uniform_int_distribution<std::mt19937::result_type> dist;
     static constexpr size_t RTM_ATTEMPTS = 3;
-    static constexpr size_t RTM_WAITNSEC = 2;
 
 public:
-    explicit HashSetGCC_RTM(size_t initCapacity = 11) : HashSet{initCapacity}, rng{std::random_device{}()} {
+    explicit HashSetGCC_RTM(size_t initCapacity = 11) : HashSet{initCapacity}, rng{std::random_device{}()}, dist(1, 50) {
 
     }
 
@@ -65,7 +64,7 @@ public:
                 _xabort(0xFF);
             }
             if((status & _XABORT_EXPLICIT) && _XABORT_CODE(status) == 0xff) {
-                std::this_thread::sleep_for(std::chrono::nanoseconds((dist(rng) % RTM_WAITNSEC) + 1));
+                std::this_thread::sleep_for(std::chrono::nanoseconds(dist(rng)));
             }
             else if(!(status & _XABORT_RETRY)) {
                 break;
@@ -92,7 +91,7 @@ public:
                 _xabort(0xFF);
             }
             if((status & _XABORT_EXPLICIT) && _XABORT_CODE(status) == 0xff) {
-                std::this_thread::sleep_for(std::chrono::nanoseconds((dist(rng) % RTM_WAITNSEC) + 1));
+                std::this_thread::sleep_for(std::chrono::nanoseconds(dist(rng)));
             }
             else if(!(status & _XABORT_RETRY)) {
                 break;
@@ -119,7 +118,7 @@ public:
                 _xabort(0xFF);
             }
             if((status & _XABORT_EXPLICIT) && _XABORT_CODE(status) == 0xff) {
-                std::this_thread::sleep_for(std::chrono::nanoseconds((dist(rng) % RTM_WAITNSEC) + 1));
+                std::this_thread::sleep_for(std::chrono::nanoseconds(dist(rng)));
             }
             else if(!(status & _XABORT_RETRY)) {
                 break;
